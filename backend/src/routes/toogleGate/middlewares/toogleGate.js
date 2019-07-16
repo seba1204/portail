4b25c356c9469f44e4fc4b67c91ee2256db8567b
Chap <seba12.04@orange.fr>
2019-07-16 21:47:44 +0200

    16

--- backend/src/routes/toogleGate/middlewares/toogleGate.js
+++ backend/src/routes/toogleGate/middlewares/toogleGate.js
@@ -7,8 +7,15 @@ export const postToogleGate = async(req, res) => {
   .then(err => {
     if (err) return res.status(500).send(err)
     else return takePhoto().then(err => {
-      if (err) return res.status(500).send({...err, description: `${err.description} but gate is toogled !`})
-      else return res.status(200).send(GPIOSCode.relayOK)
+      console.log('on sort de la fonction takePhoto')
+      if (err) {
+        console.log('on a des erreurs')
+        return res.status(500).send({...err, description: `${err.description} but gate is toogled !`})
+      }
+      else {
+        console.log('on a aucune erreur')
+        return res.status(200).send(GPIOSCode.relayOK)
+      }
     })
   })
 
