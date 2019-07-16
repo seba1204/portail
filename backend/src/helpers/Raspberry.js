b8555ade8950927776ee040d3c0f8523300c3879
Chap <seba12.04@orange.fr>
2019-07-16 21:57:05 +0200

    23

--- backend/src/helpers/Raspberry.js
+++ backend/src/helpers/Raspberry.js
@@ -30,6 +30,5 @@ export const takePhoto = async() => {
   const outputDir = `./src/images`
   const camera = new Raspistill({time:1, fileName, outputDir})
   return await camera.takePhoto()
-  .then(p => {})
-  .catch(e => (GPIOSCode.err.takePhotoError.description))
+  .catch(e => (GPIOSCode.err.takePhotoError))
 }
