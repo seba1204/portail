e0149a15f920899d3313dff21d9884e8edd04dfd
Chap <seba12.04@orange.fr>
2019-07-16 21:44:37 +0200

    14

--- backend/src/routes/toogleGate/middlewares/toogleGate.js
+++ backend/src/routes/toogleGate/middlewares/toogleGate.js
@@ -3,11 +3,10 @@ import { basicsCode, GPIOSCode } from '../../../codes'
 
 
 export const postToogleGate = async(req, res) => {
-  toogleGate()
+  return toogleGate()
   .then(err => {
     if (err) return res.status(500).send(err)
     else takePhoto().then(err => {
-      console.log(err)
       if (err) return res.status(500).send({...err, description: `${err.description} but gate is toogled !`})
       else return res.status(200).send(GPIOSCode.relayOK)
     })
