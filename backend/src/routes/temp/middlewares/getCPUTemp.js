35c464f17edc90c172d96e23055d11f71c6cfbcd
Chap <seba12.04@orange.fr>
2019-07-18 16:29:21 +0200

    16

--- backend/src/routes/temp/middlewares/getCPUTemp.js
+++ backend/src/routes/temp/middlewares/getCPUTemp.js
@@ -7,12 +7,17 @@ import { RaspCode } from '../../../codes'
 export const getCPUTemp = async(req, res) => {
   try{
     temp.measure((err, temp) => {
-        if (err) return res.status(200).send(RaspCode.err.readingCPUTempError)
-        else return res.status(200).send({temp})
+        if (err){
+          log({name:'Raspberry', status:'err', value:RaspCode.err.readingCPUTempError.description})
+          return res.status(200).send(RaspCode.err.readingCPUTempError)
+        }
+        else{
+          log({name:'Raspberry', status:'info', value:`CPU temp : ${temp}`})
+          return res.status(200).send({temp})
+        }
     })
   } catch (e) {
-    console.log('on rentre enfin dans le catch...')
-  } finally {
-    log({name: 'Raspberry', status: 'err', value: RaspCode.err.readingCPUTempError.description})
+    log({name:'Raspberry', status:'err', value:RaspCode.err.readingCPUTempError.description})
+    return res.status(200).send(RaspCode.err.readingCPUTempError)
   }
 }
