558990f8dba25f5ffff3eba276030abbb8a9e983
Chap <seba12.04@orange.fr>
2019-07-19 04:25:48 +0200

    3

--- backend/src/routes/images/middlewares/uniqueImage.js
+++ backend/src/routes/images/middlewares/uniqueImage.js
@@ -53,7 +53,7 @@ const uniqueImage = async(req, res) => {
 
     s.on('open', function () {
         res.set('Content-Type', type);
-        s.pipe(transformer).pipe(res)
+        s.pipe(res)
     })
     s.on('error', function () {
         res.set('Content-Type', 'text/plain')
