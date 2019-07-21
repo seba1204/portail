54dbea08fcf29b58970564fef5aeb190619d86da
Chap <seba12.04@orange.fr>
2019-07-22 01:15:46 +0200

    22

--- backend/src/routes/images/middlewares/uniqueImage.js
+++ backend/src/routes/images/middlewares/uniqueImage.js
@@ -17,49 +17,46 @@ var mime = {
 };
 
 const uniqueImage = async(req, res) => {
-    let file = path.join(__dirname, `../../../images/${req.params.name}`)
-    const type = mime[path.extname(file).slice(1)] || 'text/plain'
-    let { width, height, quality } = req.query
-    let options = {}
-    let transformer = sharp()
-    // on vérifie les paramètres !
-    if (width !== undefined){
-      width = Number(width)
-      if(!help.isANumber(width) || !help.isIn(width, 1, 5000))
-        return res.status(200).send(basicsCode.err.wrongQuery)
-      options = {...options, width}
-    } else if (height === undefined) options = {...options, width:300}
 
+  let file = path.join(__dirname, `../../../images/${req.params.name}`)
+  const type = mime[path.extname(file).slice(1)] || 'text/plain'
+  let { width, height, quality } = req.query
+  let options = {}
+  let transformer = sharp()
+  // on vérifie les paramètres !
+  if (width !== undefined){
+    width = Number(width)
+    if(!help.isANumber(width) || !help.isIn(width, 1, 5000))
+      return res.status(200).send(basicsCode.err.wrongQuery)
+    options = {...options, width}
+  } else if (height === undefined) options = {...options, width:300}
+  if (height !== undefined){
+    if(!help.isANumber(height) || !help.isIn(height, 1, 5000))
+      return res.status(200).send(basicsCode.err.wrongQuery)
+    options = {...options, height:Number(height)}
+    if (width !== undefined) options = {...options, position: sharp.strategy.attention}
+  }
+  if (quality !== undefined){
+    quality = Number(quality)
+    if(!help.isANumber(quality) || !help.isIn(quality, 0, 100))
+      return res.status(200).send(basicsCode.err.wrongQuery)
+  } else quality = 50
+  if (quality!==0){
+    transformer = transformer
+      .resize(options)
+      .webp({quality})
+  }
 
-    if (height !== undefined){
-      if(!help.isANumber(height) || !help.isIn(height, 1, 5000))
-        return res.status(200).send(basicsCode.err.wrongQuery)
-      options = {...options, height:Number(height)}
-      if (width !== undefined) options = {...options, position: sharp.strategy.attention}
-    }
+  const s = fs.createReadStream(file)
 
-    if (quality !== undefined){
-      quality = Number(quality)
-      if(!help.isANumber(quality) || !help.isIn(quality, 0, 100))
-        return res.status(200).send(basicsCode.err.wrongQuery)
-    } else quality = 50
-    if (quality!==0){
-      transformer = transformer
-        .resize(options)
-        .webp({quality})
-    }
-
-    const s = fs.createReadStream(file)
-
-    s.on('open', function () {
-        res.set('Content-Type', type)
-        s.pipe(transformer).pipe(res)
-    })
-    s.on('error', function () {
-        res.set('Content-Type', 'text/plain')
-        res.status(404).end(ImageCode.err.photoNotFound)
-    })
-
+  s.on('open', function () {
+      res.set('Content-Type', type)
+      s.pipe(transformer).pipe(res)
+  })
+  s.on('error', function () {
+      res.set('Content-Type', 'text/plain')
+      res.status(404).end(ImageCode.err.photoNotFound)
+  })
 
 }
 
